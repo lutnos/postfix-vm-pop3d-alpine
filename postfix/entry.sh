70861dd8fed3a7852c316bd7f8ab5437f90787fa
Dave English <dave@lutnos.com>
2016-10-07 08:41:06 +0100

    Before getting rid of supervisord

--- postfix/entry.sh
+++ postfix/entry.sh
@@ -3,6 +3,8 @@
 if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 /usr/sbin/rsyslogd -n &
 
+postconf -e myorigin=$(cat /etc/virtual/mailname)
+
 postfix start
 
 tail -f /var/log/maillog
--- postgrey/entry.sh
+++ postgrey/entry.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# remote syslog server to docker host
+SYSLOG=`netstat -rn|grep ^0.0.0.0|awk '{print $2}'`
+echo "*.*	@$SYSLOG" > /etc/rsyslog.d/50-default.conf
+
 /usr/bin/supervisord -c /etc/supervisord.d/supervisord.ini
