221869c7a430b150ce517467fc17827ce1f5ff08
Dave English <dave@lutnos.com>
2016-10-07 17:58:54 +0100

    Interim

--- postfix/entry.sh
+++ postfix/entry.sh
@@ -3,8 +3,7 @@
 if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 /usr/sbin/rsyslogd -n &
 
-postconf -e myorigin=$(cat /etc/virtual/mailname)
-
+if [ -f /etc/virtual/mailname ]; then postconf -e myorigin=$(cat /etc/virtual/mailname); fi
 postfix start
 
 tail -f /var/log/maillog
