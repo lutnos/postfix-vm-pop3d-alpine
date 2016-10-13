b60a21b97590e6c35a3f80f2677cd6ed488086ba
Dave English <dave@lutnos.com>
2016-10-13 12:24:17 +0100

    Simplified config installation

--- spamassassin/entry.sh
+++ spamassassin/entry.sh
@@ -5,7 +5,7 @@ if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 
 # /usr/bin/sa-update --verbose
 
-/usr/sbin/spamd --daemonize --listen=0.0.0.0:783 --allowed-ips=172.16.0.0/12 --pidfile /var/run/spamd.pid \&
-  --syslog=mail --username=spamd --nouser-config --max-children=5 --create-prefs --helper-home-dir &
+/usr/sbin/spamd --daemonize --listen=0.0.0.0:783 --allowed-ips=172.16.0.0/12 --pidfile /var/run/spamd.pid \
+  --syslog=spamassassin --username=spamd --nouser-config --max-children=5 --create-prefs --helper-home-dir &
 
-tail -f /var/log/maillog
+tail -f /var/log/messages
