036889f2cff0226eed2f9f6684a6ecf866c32229
Dave English <dave@lutnos.com>
2018-09-09 21:02:07 +0100

    Remove erroneous postgrey logging facility

--- postgrey/entry.sh
+++ postgrey/entry.sh
@@ -4,6 +4,6 @@ if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 /usr/sbin/rsyslogd -n &
 
 /usr/sbin/postgrey --daemonize --inet=0.0.0.0:10023 --pidfile=/var/run/postgrey.pid --delay=50 \
-  --syslog-facility postgrey --hostname=postgrey --greylist-text='Greylisted for %s seconds'
+  --hostname=postgrey --greylist-text='Greylisted for %s seconds'
 
 tail -f /var/log/messages
