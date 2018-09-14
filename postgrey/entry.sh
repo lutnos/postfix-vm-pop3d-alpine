2b50cf093ef379397c929de9e88493b4199fe7c6
Dave English <dave.english@digital.hmrc.gov.uk>
2018-09-14 17:15:46 +0100

    Remove pids in entry.sh scripts

--- postgrey/entry.sh
+++ postgrey/entry.sh
@@ -3,6 +3,7 @@
 if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 /usr/sbin/rsyslogd -n &
 
+if [ -f /var/run/postgrey.pid ]; then rm /var/run/postgrey.pid; fi
 /usr/sbin/postgrey --daemonize --inet=0.0.0.0:10023 --pidfile=/var/run/postgrey.pid --delay=50 \
   --hostname=postgrey --greylist-text='Greylisted for %s seconds'
 
