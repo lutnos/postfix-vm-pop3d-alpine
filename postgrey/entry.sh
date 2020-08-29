50f70d97f8727a8909e9d66a127ab32c50ea9ccf
Dave English <4254983+lutnos@users.noreply.github.com>
2020-08-29 10:14:33 +0100

    move tuneable config to a volume

--- postgrey/entry.sh
+++ postgrey/entry.sh
@@ -4,6 +4,12 @@ if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 /usr/sbin/rsyslogd -n &
 
 if [ -f /var/run/postgrey.pid ]; then rm /var/run/postgrey.pid; fi
+
+if [ ! -f /etc/tuning/postgrey_whitelist_clients ]; then cp /etc/volume/postgrey_whitelist_clients /etc/tuning; fi
+ln -sf /etc/tuning/postgrey_whitelist_clients /etc/postfix
+if [ ! -f /etc/tuning/postgrey_whitelist_recipients ]; then cp /etc/volume/postgrey_whitelist_recipients /etc/tuning; fi
+ln -sf /etc/tuning/postgrey_whitelist_recipients /etc/postfix
+
 /usr/sbin/postgrey --daemonize --inet=0.0.0.0:10023 --pidfile=/var/run/postgrey.pid --delay=50 \
   --hostname=postgrey --greylist-text='Greylisted for %s seconds'
 
--- spamassassin/entry.sh
+++ spamassassin/entry.sh
@@ -6,6 +6,10 @@ if [ -f /var/run/rsyslogd.pid ]; then rm /var/run/rsyslogd.pid; fi
 # /usr/bin/sa-update --verbose
 
 if [ -f /var/run/spamd.pid ]; then rm /var/run/spamd.pid; fi
+
+if [ ! -f /etc/tuning/local.cf ]; then cp /etc/volume/local.cf /etc/tuning; fi
+ln -sf /etc/tuning/local.cf /etc/mail/spamassassin
+
 /usr/sbin/spamd --daemonize --listen=0.0.0.0:783 --allowed-ips=172.16.0.0/12 --pidfile /var/run/spamd.pid \
   --syslog=spamassassin --username=spamd --nouser-config --max-children=5 --create-prefs --helper-home-dir &
 
