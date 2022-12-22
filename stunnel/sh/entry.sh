2a89fe1b96e51b2162c4605b711760c300fa0ccb
lutnos <4254983+lutnos@users.noreply.github.com>
2022-12-22 16:28:42 +0000

    persist stunnel configuration

--- stunnel/sh/entry.sh
+++ stunnel/sh/entry.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+if [ ! -d /etc/volume/stunnel ]; then mkdir /etc/volume/stunnel && cp /etc/entry/* /etc/volume/stunnel; fi
+ln -sf /etc/volume/stunnel /etc
+
 stunnel
 
 tail -f /var/stunnel/stunnel.log
